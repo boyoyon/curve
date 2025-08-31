c64dfc36aa937087e0f8eb022145131e31a10371
boyoyon <nishimaki.hidetoshi@gmail.com>
2025-08-31 22:54:34 +0900

    Add files via upload

---
+++ src/ode_curve_plot_continuous_shooting.py
@@ -0,0 +1,34 @@
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+#Define function
+def odef(xyz,t,sigma,rho,beta):
+    x,y,z = xyz
+    return [sigma*(y-x),x*(rho-z)-y,x*y -beta*z]
+
+#Create data
+t = np.linspace(0,25,10000)
+xyz0 = [1,1,1]
+sigma, rho, beta = 8, 28, 8/3
+
+#Integrate under function
+xyz = odeint(odef,xyz0,t, args=(sigma,rho,beta))
+
+#Create figure
+
+ax = plt.axes(projection='3d')
+ax.set_aspect('equal')
+ax.plot(xyz[:,0],xyz[:,1],xyz[:,2],alpha=0.5)
+plt.tight_layout()
+
+elevation = 30
+
+for i, azimuth in enumerate(range(0, 360, 5)):
+
+    ax.view_init(elev=elevation, azim=azimuth)
+
+    dst_path = '%04d.png' % (i+1)
+    plt.savefig(dst_path)
+    print('save %s' % dst_path)
